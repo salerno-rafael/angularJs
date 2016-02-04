625b50da9208fe85675972e133d70b6881748559
Rafael Salerno <salerno.rafael@gmail.com>
2016-02-04 10:56:02 -0200

    new sample

---
+++ sample-twelve/app.js
@@ -0,0 +1,66 @@
+(function(){
+	var app = angular.module('store',[]);
+
+	app.controller('StoreController', function(){
+		this.products = gems; 
+	});
+
+	app.controller('CommentController', function(){
+		this.comment ={};
+		
+		this.addComment = function(product){
+			product.comments.push(this.comment);
+			this.comment ={};
+		};
+
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	var gems = [  
+	{
+		name:'meias',
+		price: 1.99,
+		description: 'meia branca tamanho 20',
+		canPurchase : true,
+		image:'meia.jpg',
+		comments:[
+			{
+				stars:5,
+				body:'produto bom hein o Batista',
+				author:'salerno.rafael@gmail.com'
+			},
+			{
+				stars:2,
+				body:'produto bem meia boca',
+				author:'salerno.rafael@gmail.com'
+			}
+		]
+	},
+	{
+		name:'camisetas',
+		price: 8.99,
+		description: 'tamanho 3 azul',
+		canPurchase : true,
+		image:'camiseta.jpg',
+		comments:[
+			{
+				stars:3,
+				body:'produto de qualidade',
+				author:'salerno.rafael@gmail.com'
+			}
+		]
+	}
+	];
+
+})();
