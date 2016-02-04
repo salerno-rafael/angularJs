fc8cac6174a1ec23ed2374b9dcfa37b8b8c7888d
Rafael Salerno <salerno.rafael@gmail.com>
2016-02-04 17:52:40 -0200

    sample

---
+++ sample-sixteen/app.js
@@ -0,0 +1,27 @@
+(function(){
+	var app = angular.module('store',['panel-module']); //store depende do panel-module
+
+	app.controller('StoreController', function(){
+		this.products = gems; 
+	});
+
+	var gems = [  
+	{
+		name:'meias',
+		price: 1.99,
+		description: 'meia branca tamanho 20',
+		comments: 'produto bom hein o Batista',
+		canPurchase : true,
+		image:'meia.jpg'
+	},
+	{
+		name:'camisetas',
+		price: 8.99,
+		description: 'tamanho 3 azul',
+		comments: 'produto bom hein o Batista',
+		canPurchase : true,
+		image:'camiseta.jpg'
+	}
+	];
+
+})();
